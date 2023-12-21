eaba5266657a64b6c8fd293737e5d57312ca7ccb
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-12-21 20:39:52 +0100

    add tutorials files

---
+++ tutorial/tmdbsimple_tutorial.py
@@ -0,0 +1,13 @@
+import tmdbsimple as tmdb
+
+tmdb.API_KEY = '454b6ca4172e455fe7a7d8395c10d6d9'
+
+search = tmdb.Search()
+
+response = search.movie(query='Aliens')['results'][0]
+
+import pprint
+
+pprint.pprint(response, indent=4)
+
+"https://image.tmdb.org/t/p/original/r1x5JGpyqZU8PYhbs4UcrO1Xb6x.jpg"
