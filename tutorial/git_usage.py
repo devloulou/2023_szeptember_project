32fa722993bf877b9262874ac19005a3e1fa34e8
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-12-21 19:33:23 +0100

    finalize git_usage.py

--- tutorial/git_usage.py
+++ tutorial/git_usage.py
@@ -15,9 +15,12 @@ Amikor még nem git kezelt a kódod:
 3. initial commit - ő a mentett állapot
     - a változásokat stage-elni kell -> ez után már commitálni tudsz -> elmented az állapotot
     - commit-olom a változást: adok egy commit üzenetet, lerögzítem az állapotot
+4. létrehozok egy remote branchet pl. a github-on és fel push-olom a meglévő kódomat
+
 Amikor már git kezelt:
 -> létrehozol valamilyen változást a kódbázison: (új sor, új karakter, új file, új mappa)
--> ezeket a változásokat figyelni akarod: git add . 
+-> ezeket a változásokat figyelni akarod: stageled a változást
     - azt az állapotot, amit figyelek ahhoz mérten fogom tudni a visszaállítást alkalmazni
     - vagy a flow további lépcsőjére lépni: -
+-> commit message és commit
 """
