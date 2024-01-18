f0f9615caa16a81d9adb84449ad886a404b45d9b
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2024-01-18 17:55:03 +0100

    30. alkalom

---
+++ tutorial/mongo_db/tutorial.py
@@ -0,0 +1,18 @@
+"""
+NoSQL - Not Only SQL
+
+ - documentum based - MongoDB - file-okat fogunk tárolni - tipikisan JSON-t
+ - key - value stores - Redis (in-memory db - memóriában tárolja az adatot ) - 
+ - graph - pl SAP HANA - az adatok matematikai gráf szerkezetben vannak elrendezve
+ - Column-oriented databases - Redshift, HBase
+
+
+MongoDB
+
+NoSQL adatbázis - Nem relációs adatbázis -> nem táblákba szervezem az adatot
+nem lesz kiépített kapcsolat a táblák között -> szülő - gyermek kapcsolat
+
+
+
+
+"""
